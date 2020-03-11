c99ab2f388a8dce3eb2acfa8efaed5dacab2703b
Daniel Walker <roshiredyoshi@gmail.com>
2020-03-11 10:57:22 -0700

    Create_StandardGradeBook_class_1

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        private string Name;
+
+        public StandardGradeBook : base(name)
+        {
+            Name = name;
+        }
+
+    }
+}

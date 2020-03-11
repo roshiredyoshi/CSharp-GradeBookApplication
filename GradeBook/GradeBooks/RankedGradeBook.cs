6a0163db00dd918d8f205cbae8b15f7e9abda920
Daniel Walker <roshiredyoshi@gmail.com>
2020-03-11 12:22:17 -0700

    Update RankedGradeBook.cs
    Section 3 Step 1.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -6,10 +6,42 @@ namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
+        public double TwentyPercent = 0;
+        // public List<double> Grades;
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
         }
+        
+        public override char GetLetterGrade(double averageGrade)
+        {
+            // Return char letter grade
+            if (Students.Count <= 5)
+            {
+                throw InvalidOperationException("Ranked-grading requires a minimum of 5 students to work.");
+            }
+            // TwentyPercent = (Students.Count * .2);
+            
+            return 'F';
+            
+            //else if (true)
+            //{
+            //    return 'B';
+            //}
+            //else if (true)
+            //{
+            //    return 'C';
+            //}
+            //else if (true)
+            //{
+            //    return 'D';
+            //}
+            //return 'F';
+        }
 
+        private Exception InvalidOperationException(string v)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
